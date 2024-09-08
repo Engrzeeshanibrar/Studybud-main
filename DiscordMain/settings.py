88b67b8e0ec8477c4f9d1dc84df1bb8b9e2ea935
zeeshanibrarali <zeeshanibrar10987@gmail.com>
2024-09-08 23:56:14 +0500

    push

--- DiscordMain/settings.py
+++ DiscordMain/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-d6#-s0(q^l%89=92kmg!sn5(^)t)f56abx%$3g@_2f1n*$c^p$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
