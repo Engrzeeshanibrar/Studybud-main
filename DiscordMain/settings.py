2d66d8477a6ad086a8c54ef815983383871d6e71
zeeshanibrarali <zeeshanibrar10987@gmail.com>
2024-09-08 22:54:31 +0500

    push

--- DiscordMain/settings.py
+++ DiscordMain/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
+    "whitenoise.runserver_nostatic",
     "base.apps.BaseConfig",
 ]
 
@@ -40,6 +40,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "DiscordMain.urls"
--- DiscordMain/urls.py
+++ DiscordMain/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf import settings # new
+from  django.conf.urls.static import static #new
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -9,3 +11,7 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
